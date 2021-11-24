b9b7bc3ede2d3ad2c111d2ce5ecd413cc313baee
ale2fernandez2003 <92783863+ale2fernandez2003@users.noreply.github.com>
2021-11-24 10:23:25 +0100

    primer commit

---
+++ teclado.java
@@ -0,0 +1,36 @@
+
+/**
+ * Write a description of class teclado here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class teclado
+{
+    // instance variables - replace the example below with your own
+    private int numeroTeclas;
+    
+    private boolean conectado;
+
+    /**
+     * Constructor for objects of class teclado
+     */
+    public teclado()
+    {
+        // initialise instance variables
+        numeroTeclas = 105;
+        conectado=true;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
