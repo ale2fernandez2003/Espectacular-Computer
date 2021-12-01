7f36ddf34d9165592479dba834d1bbde3c269303
ale2fernandez2003 <92783863+ale2fernandez2003@users.noreply.github.com>
2021-12-01 10:04:11 +0100

    5.0 cree nuevos atributos en la clase ordenador

--- ordenador.java
+++ ordenador.java
@@ -18,4 +18,36 @@ public class Ordenador {
         id = codigoOrdenador;
         teclado = elTeclado;
     }
+    
+    /**
+     *
+     */
+    public String getMarca() {
+        return marca;
+    }
+    
+    /**
+     *
+     */
+    public long getId () {
+        return id;
+    }
+    
+    /**
+     *
+     */
+    public void setCodigoOrdenador (long codigoDelOrdenador) {
+        long codigoOrdenador= codigoDelOrdenador;
+    }
+    
+    public void setFabricante (String elFabricante) {
+        String fabricante= elFabricante;
+    }
+    
+    /**
+     *
+     */
+    public Teclado getelTeclado() {
+        return teclado;
+    }
 }
